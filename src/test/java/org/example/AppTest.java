b06910df8f31bacfddd75b7c6fb3ad34951d7b4a
clarayuan <c_w_yuan@hotmail.com>
2024-01-08 16:43:14 -0600

    first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    void basictest() {
+        assertThat(true).isTrue();
+    }
+}
